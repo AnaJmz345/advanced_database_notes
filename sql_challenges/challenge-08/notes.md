cc34927ef4dd4192e5a8f9ab379d62c9b0228f58
Ana Jmz <143899647+AnaJmz345@users.noreply.github.com>
2026-04-26 16:26:09 -0600

    "excercise 2"

--- sql_challenges/challenge-08/notes.md
+++ sql_challenges/challenge-08/notes.md
@@ -9,3 +9,19 @@ WHERE site_id = 3;
 
 
 ## Excercise 2 - 
+
+```sql
+CREATE INDEX idx_pv_visit_date
+ON patient_visits(visit_date);
+
+BEGIN
+    DBMS_STATS.GATHER_TABLE_STATS(USER, 'PATIENT_VISITS', cascade => TRUE);
+END;
+/
+```
+In this excercise I created an index and executed the gather stats stament. 
+
+### Why is it important to execute gather stats?
+Gathering stats is how Oracle learns about the table. When you run DBMS_STATS.GATHER_TABLE_STATS, the database analyzes the data and collects information like how many rows there are, how values are distributed, and how selective each column is. The query optimizer uses these statistics to decide the best execution plan. For example, it decides whether to use an index or do a full table scan. Without accurate stats, Oracle is guessing, and it may choose a bad plan even if an index exists. So even if you create an index, Oracle might not use it unless the stats are updated. That’s why after inserting data or creating indexes, we run gather stats again, so the optimizer has correct information and can make better decisions.
+
+Also I saw first hand how Oracle really analyzes the effectiveness and doesn't always use indexes only for being created and available, it chooses the best option.
--- sql_challenges/challenge-08/solution.sql
+++ sql_challenges/challenge-08/solution.sql
@@ -11,3 +11,51 @@ SELECT * FROM patient_visits WHERE site_id = 3;
 -- c) Would adding an index on site_id help? Why or why not?
 -- No, due to its low cardinality,  many rows match the condition (site_id = 3). So, the database would still need to access a 
 -- large portion of the table, making a full table scan more efficient than using the index.
+
+-- Exercise 2 — Create an index and see if it helps
+
+-- 1. Create an index on visit_date.
+CREATE INDEX idx_pv_visit_date
+ON patient_visits(visit_date);
+
+-- 2.Gather stats
+BEGIN
+    DBMS_STATS.GATHER_TABLE_STATS(USER, 'PATIENT_VISITS', cascade => TRUE);
+END;
+/
+
+-- 3. Run the query
+SELECT * FROM patient_visits
+WHERE visit_date BETWEEN SYSDATE - 30 AND SYSDATE;
+
+-- 4. Questions
+
+-- a) Does Oracle use the index for this range?
+
+EXPLAIN PLAN FOR
+SELECT * FROM patient_visits
+WHERE visit_date BETWEEN SYSDATE - 30 AND SYSDATE;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY); 
+
+--With this query, it returned that Oracle didn't use the index because it showed TABLE ACCESS FULL,
+-- and that Oracle estimated that the query would return around 2,125 rows, so it decided that scanning the full table 
+-- was cheaper than using the index and then accessing the table rows. This shows that creating an index does not guarantee
+-- that Oracle will use it; the optimizer chooses the plan with the lowest estimated cost.
+
+
+-- b) Change the range to the last 7 days. Does the plan change?
+-- The execution plan shows a TABLE ACCESS FULL again, even though the estimated rows decreased from around 2,125 rows to 547 rows.
+-- This means that Oracle still considered the full table scan cheaper than using the visit_date index. So the range became more selective,
+-- but not enough for the optimizer to choose the index.
+
+-- c) Change to the last 700 days. What happens?
+--The query returns a much larger number of rows, almost the entire table. In this case, Oracle does not use the index and 
+-- performs a TABLE ACCESS FULL, because using the index would require accessing many rows anyway, making it less efficient than 
+-- scanning the whole table directly.
+
+-- d) Why does the range size affect whether Oracle uses the index?
+--The range size affects selectivity. Smaller ranges return fewer rows, so the index helps reduce the amount of data that needs
+-- to be read. However, larger ranges return many rows, so the benefit of the index decreases. At that point, Oracle prefers a 
+-- full table scan because it is cheaper than using the index and then accessing many rows from the table.
+--In this specific case, Oracle used a full table scan in all scenarios because the table is relatively small, so scanning the
+-- entire table is still cheaper than using the index, even for smaller ranges.
